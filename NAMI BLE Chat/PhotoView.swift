cd782f98acf8d8bf6e8933c73462d9bf2d2363dd
Tadashi Ogino <tadahi.ogino@nifty.com>
2023-11-12 20:57:38 +0900

    Linkがmessageに表示されて、Linkの関数が呼ばれるところまで完成

---
+++ NAMI BLE Chat/PhotoView.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoView.swift
+//  NAMI BLE Chat
+//
+//  Created by Tadashi Ogino on 2023/11/11.
+//
+
+import SwiftUI
+
+struct PhotoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoView()
+}
