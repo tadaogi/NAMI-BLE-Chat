b4c913a4fb50529f708a759fed9a482ff19d3905
Tadashi Ogino <tadahi.ogino@nifty.com>
2023-11-17 20:29:20 +0900

    写真のアップロード、リンクの表示、リンク先がない時の処理、の追加

---
+++ NAMI BLE Chat/PhotoShow.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoShow.swift
+//  NAMI BLE Chat
+//
+//  Created by Tadashi Ogino on 2023/11/15.
+//
+
+import SwiftUI
+
+struct PhotoShow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoShow()
+}
